56863a752dde009b487263d4f4e750619e741378
mirko-porcu-mindtek <mirko.porcu@mindtek.it>
2018-03-08 12:00:29 +0100

    set color of section

--- src/app/work/section-solver.service.ts
+++ src/app/work/section-solver.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { Params } from '@angular/router';
 
 const section = [
-  { id: 0, name: 'accoglienza', background: '#40ACCC' },
-  { id: 1, name: 'il-lavoro', background: '#40ACCC' },
-  { id: 2, name: 'il-cibo',background: '#40ACCC' },
-  { id: 3, name: 'pronto', background: '#40ACCC' },
-  { id: 4, name: 'come-sei-come-stai', background: '#40ACCC' },
-  { id: 5, name: 'la-salute', background: '#40ACCC' },
-  { id: 6, name: 'la-citta', background: '#40ACCC' },
-  { id: 7, name: 'mezzi-di-trasporto', background: '#40ACCC' },
-  { id: 8, name: 'la-cura-della-casa', background: '#40ACCC' },
-  { id: 9, name: 'il-viaggio', background: '#40ACCC' }
+  { id: 0, name: 'accoglienza', background: '#37ABDF' },
+  { id: 1, name: 'il-lavoro', background: '#2C74B6' },
+  { id: 2, name: 'il-cibo',background: '#72458E' },
+  { id: 3, name: 'pronto', background: '#AE007D' },
+  { id: 4, name: 'come-sei-come-stai', background: '#D4145D' },
+  { id: 5, name: 'la-salute', background: '#D2262D' },
+  { id: 6, name: 'la-citta', background: '#DD6927' },
+  { id: 7, name: 'mezzi-di-trasporto', background: '#EAAC12' },
+  { id: 8, name: 'la-cura-della-casa', background: '#9ACB2A' },
+  { id: 9, name: 'il-viaggio', background: '#139B43' }
 ]
 
 export class Section {
