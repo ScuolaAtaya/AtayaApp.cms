7b0f2d7df548f5b20f3b80591407336823b56586
Giovanni Liboni <giovanni@liboni.net>
2018-03-08 11:29:44 +0100

    Updated src/app/work/work.service.ts

--- src/app/work/work.service.ts
+++ src/app/work/work.service.ts
@@ -13,32 +13,32 @@ export abstract class WorkService {
     protected target: string;
 
     protected getAll<T extends Work>(): Observable<T[]> {
-        return this.http.get(this.createUrl(this.baseUrl, this.target), this.getHttpOptions())
+        return this.http.get(this.createUrl(this.baseUrl, this.target))
             .map(response => response as T[]);
     }
 
     getOne<T extends Work>(id: string): Observable<T> {
-        return this.http.get(this.createUrl(this.baseUrl, this.target, id), this.getHttpOptions())
+        return this.http.get(this.createUrl(this.baseUrl, this.target, id))
             .map(response => response as T);
     }
 
     protected getAllBySection<T extends Work>(section: number): Observable<T[]> {
-        return this.http.get(this.createUrl(this.baseUrl, this.target, 'unit_id', String(section)), this.getHttpOptions())
+        return this.http.get(this.createUrl(this.baseUrl, this.target, 'unit_id', String(section)))
             .map(response => response as T[]);
     }
 
     update<T extends Work>(work: T, id: string) {
-        return this.http.put(this.createUrl(this.baseUrl, this.target, id), JSON.stringify(work), this.getHttpOptions())
+        return this.http.put(this.createUrl(this.baseUrl, this.target, id), JSON.stringify(work), {})
             .map(res => res);
     }
 
     create<T extends Work>(work: T) {
-        return this.http.post(this.createUrl(this.baseUrl, this.target), JSON.stringify(work), this.getHttpOptions())
+        return this.http.post(this.createUrl(this.baseUrl, this.target), JSON.stringify(work), {})
             .map(res => res);
     }
 
     delete(id: string) {
-        return this.http.delete(this.createUrl(this.baseUrl, this.target, id), this.getHttpOptions())
+        return this.http.delete(this.createUrl(this.baseUrl, this.target, id), {})
             .map(res => res)
     }
 
@@ -50,17 +50,6 @@ export abstract class WorkService {
 
     protected createUrl(...elements: string[]): string {
         return elements.join('/');
-    }
-
-    protected getHttpOptions() {
-        return {
-            headers: new HttpHeaders(
-                {
-                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YTlkMzg1NmZhYzdjZmM1YTQwNGNiYzkiLCJpYXQiOjE1MjAzNDk2OTV9.9c0UWfoXbaKmiqEP1UFH_h3fRviNwfTunBRaPd1n2ZY',
-                    'Content-Type': 'application/json; charset=utf-8'
-                }
-            )
-        };
     }
 }
 
