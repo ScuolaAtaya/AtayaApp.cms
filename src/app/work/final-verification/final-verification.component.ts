5f5ff546a5e02731573c5f530ac127b0e333be91
Andrea Magoni <andrea.magoni@mindtek.it>
2020-06-04 13:24:45 +0200

    First commit

---
+++ src/app/work/final-verification/final-verification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ms-final-verification',
+  templateUrl: './final-verification.component.html',
+  styleUrls: ['./final-verification.component.scss']
+})
+export class FinalVerificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/work/final-verification/final-verification.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
+import { DragulaModule } from 'ng2-dragula/ng2-dragula';
+import { SortablejsModule } from "angular-sortablejs";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FileUploadModule } from 'ng2-file-upload/ng2-file-upload';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from 'ng2-translate/ng2-translate';
+import { CommonComponentModule } from '../../common/common.module';
+import { FinalVerificationComponent } from './final-verification.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    DragulaModule,
+    SortablejsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FileUploadModule,
+    NgxDatatableModule,
+    HttpClientModule,
+    RouterModule,
+    TranslateModule.forRoot(),
+    CommonComponentModule
+  ],
+  declarations: [FinalVerificationComponent],
+  providers: []
+})
+export class WriteModule { }
