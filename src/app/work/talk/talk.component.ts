d19792290d8d02ac5b416780ecd1edcb4bb59d50
Andrea Magoni <andrea.magoni@mindtek.it>
2020-06-17 10:03:11 +0200

    Updated talk component

--- src/app/work/talk/talk.component.ts
+++ src/app/work/talk/talk.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { PageTitleService } from '../../core/page-title/page-title.service';
-import { SortablejsOptions } from "angular-sortablejs";
-import { fadeInAnimation } from "../../core/route-animation/route.animation";
-import { FormTalkComponent } from './form-talk/form-talk.component'
+import { SortablejsOptions } from 'angular-sortablejs';
+import { fadeInAnimation } from '../../core/route-animation/route.animation';
 import { TalkService } from './talk.service';
 import { Talk } from './talk';
 import { ActivatedRoute } from '@angular/router';
@@ -11,85 +10,78 @@ import { Router } from '@angular/router'
 import { environment } from 'environments/environment';
 import { WorkListMenuItems } from '../work-list-menu-items';
 import { UtilsService } from './../../common/utils.service';
-import {TranslateService} from "ng2-translate";
+import { TranslateService } from 'ng2-translate';
+import { Media } from '../media';
 
 @Component({
-	selector: 'ms-talk',
-	templateUrl: './talk.component.html',
-	styleUrls: ['./talk.component.scss'],
-	encapsulation: ViewEncapsulation.None,
-	host: {
-		"[@fadeInAnimation]": 'true'
-	},
-	animations: [fadeInAnimation]
+  selector: 'ms-talk',
+  templateUrl: './talk.component.html',
+  styleUrls: ['./talk.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    '[@fadeInAnimation]': 'true'
+  },
+  animations: [fadeInAnimation]
 })
 export class TalkComponent implements OnInit {
+  talkList: Talk[];
+  section: Section;
+  groupOptions: SortablejsOptions = {
+    group: 'testGroup',
+    handle: '.drag-handle',
+    animation: 300
+  };
+  simpleOptions: SortablejsOptions = {
+    animation: 300
+  };
 
-	talkList: Talk[];
+  constructor(
+    private talkService: TalkService,
+    private pageTitleService: PageTitleService,
+    private route: ActivatedRoute,
+    private sectionService: SectionSolverService,
+    private router: Router,
+    public workListMenuItems: WorkListMenuItems,
+    public utils: UtilsService,
+    private translate: TranslateService
+  ) { }
 
-	groupOptions: SortablejsOptions = {
-		group: 'testGroup',
-		handle: '.drag-handle',
-		animation: 300
-	};
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.section = this.sectionService.retrieveSection(params);
+      this.translate.get('Parliamo').subscribe((translatedText: string) => this.pageTitleService.setTitle(translatedText));
+      this.downloadData();
+    });
+  }
 
-	simpleOptions: SortablejsOptions = {
-		animation: 300
-	};
+  menuAction(item, menutItem) {
+    const type = menutItem.type;
+    const id = item._id;
+    if (type === 'delete') {
+      this.utils.confirm('Sei sicuro di voler continuare?').subscribe(result => {
+        if (result) {
+          this.talkService.delete(id).subscribe(
+            res => {
+              console.log(res)
+              this.downloadData();
+            },
+            err => console.log('Error occured : ' + err)
+          );
+        }
+      });
+    } else {
+      this.router.navigate([this.section.name + '/talk/exercise', id]);
+    }
+  }
 
-	section: Section;
+  getMediaUrl(picture: Media) {
+    return environment.baseUrlImage + '/' + picture.value;
+  }
 
-	constructor(private talkService: TalkService,
-		private pageTitleService: PageTitleService,
-		private route: ActivatedRoute,
-		private sectionService: SectionSolverService,
-		private router: Router,
-		public workListMenuItems: WorkListMenuItems,
-		public utils: UtilsService,
-		private translate: TranslateService
-	) { }
-
-	ngOnInit() {
-		this.route.params.subscribe(params => {
-			this.section = this.sectionService.retrieveSection(params);
-			this.translate.get('Parliamo').subscribe((translatedText: string) => {
-				this.pageTitleService.setTitle(translatedText);
-			})
-			this.downloadData()
-		});
-	}
-
-	menuAction(item, menutItem) {
-		let type = menutItem.type
-		let id = item._id
-		if (type == 'delete') {
-			this.utils.confirm('Sei sicuro di voler continuare?').subscribe(result => {
-				if (result) {
-					this.talkService.delete(id).subscribe(
-						res => {
-							console.log(res)
-							this.downloadData();
-						},
-						err => console.log('Error occured : ' + err)
-					)
-				}
-			})
-		}
-		else {
-			this.router.navigate([this.section.name + '/talk/exercise', id])
-		}
-	}
-
-	getMediaUrl(fileName) {
-		return environment.baseUrlImage + '/' + fileName
-	}
-
-	downloadData() {
-		this.talkService.getList(this.section.id)
-		.subscribe(
-			res => this.talkList = res as Talk[],
-			err => console.log('Error occured : ' + err)
-		);
-	}
+  downloadData() {
+    this.talkService.getList(this.section.id).subscribe(
+      res => this.talkList = res as Talk[],
+      err => console.log('Error occured : ' + err)
+    );
+  }
 }
-
