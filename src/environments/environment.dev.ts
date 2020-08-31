4ee2c316c1df0292b00d69b6c440f41eec2c843f
Andrea Magoni <andrea.magoni@mindtek.it>
2020-08-31 10:54:26 +0200

    Updated environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:5000/api/cms',
-  baseUrlImage: 'http://localhost:5000/api/cms/media'
+  baseUrl: 'http://api-dev.ruah.mindtek.it/api/cms',
+  baseUrlImage: 'http://api-dev.ruah.mindtek.it/api/cms/media'
 };
