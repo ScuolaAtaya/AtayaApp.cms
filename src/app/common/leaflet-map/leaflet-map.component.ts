137f8856938a82d5a977b56b1adab4549beaaa12
Andrea Magoni <andrea.magoni@mindtek.it>
2020-09-01 12:34:17 +0200

    Updated leaflet-map component

--- src/app/common/leaflet-map/leaflet-map.component.ts
+++ src/app/common/leaflet-map/leaflet-map.component.ts
@@ -1,6 +1,5 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {CRS, icon, imageOverlay, LatLng, map, marker} from 'leaflet';
-import * as $ from 'jquery';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CRS, icon, imageOverlay, LatLng, map, marker } from 'leaflet';
 
 @Component({
   selector: 'ms-leaflet-map',
@@ -8,135 +7,122 @@ import * as $ from 'jquery';
   styleUrls: ['./leaflet-map.component.scss']
 })
 export class LeafletMapComponent implements OnInit {
-  lfMap?: any;
-  _url = '';
-  _markers: Marker[];
   @Output() onMarkerDragEnd = new EventEmitter<Marker>();
   @Output() onMarkerClick = new EventEmitter<Marker>();
   @Output() onMapClick = new EventEmitter<[number, number]>();
-  _onMapClick = (event) => {
-    const latlng = event.latlng
-    this.onMapClick.emit([latlng.lng, latlng.lat])
-  };
-  leafletMarkers: any[] = []
+  lfMap?: any;
+  _url = '';
+  _markers: Marker[];
+  leafletMarkers: any[] = [];
+  _onMapClick = (event: any) => this.onMapClick.emit([event.latlng.lng, event.latlng.lat]);
 
   constructor() {
-    $(document).ready(() => {
-      this.initMap()
-    })
+    this.initMap();
   }
 
   @Input()
   set url(url: string) {
-    this._url = url
-    this.initMap()
+    this._url = url;
+    this.initMap();
   }
 
   @Input()
   set markers(markers: Marker[]) {
-    this._markers = markers
-    this.initMap()
+    this._markers = markers;
+    this.initMap();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   async initMap() {
     try {
       if (this.lfMap != null) {
-        this.lfMap.off('click', this._onMapClick)
-        this.lfMap.off()
-        this.lfMap.remove()
+        this.lfMap.off('click', this._onMapClick);
+        this.lfMap.off();
+        this.lfMap.remove();
       }
-      this.lfMap = map('map', {crs: CRS.Simple, minZoom: -3})
-      await this.loadMap(this._url)
-      this.removeMarkers()
-      this._markers.forEach((marker) => {
-        this.addMarker(marker)
-      })
-    }
-    catch(e) {
+      this.lfMap = map('map', { crs: CRS.Simple, minZoom: -3 });
+      await this.loadMap(this._url);
+      this.removeMarkers();
+      this._markers.forEach(marker => this.addMarker(marker));
+    } catch (e) {
+      console.log(e);
     }
   }
 
   removeMarkers() {
     if (this.lfMap != null) {
-      this.leafletMarkers.forEach((layer) => {
-        this.lfMap.removeLayer(layer);
-      });
+      this.leafletMarkers.forEach(layer => this.lfMap.removeLayer(layer));
     }
     this.leafletMarkers = [];
   }
 
   addMarker(inputMarker: Marker) {
-    const latlng = new LatLng(inputMarker.y, inputMarker.x)
+    const latlng = new LatLng(inputMarker.y, inputMarker.x);
     const newMarker = marker(
       latlng,
       {
         draggable: true,
         icon: icon({
-          iconSize: [ 25, 41 ],
-          iconAnchor: [ 13, 41 ],
+          iconSize: [25, 41],
+          iconAnchor: [13, 41],
           iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/marker-icon.png',
           shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/marker-shadow.png'
         })
       }
-    )
+    );
     if (inputMarker.name != null) {
-      newMarker.bindTooltip(inputMarker.name, { direction: 'top',  offset: [ 0, -41 ] })
+      newMarker.bindTooltip(inputMarker.name, { direction: 'top', offset: [0, -41] });
     }
     newMarker.on('dragend', () => {
-      const newPosition = newMarker.getLatLng()
-      inputMarker.x = newPosition.lng
-      inputMarker.y = newPosition.lat
-      this.onMarkerDragEnd.emit(inputMarker)
-    })
-    newMarker.on('click', () => {
-      this.onMarkerClick.emit(inputMarker)
-    })
-    newMarker.addTo(this.lfMap)
-    this.leafletMarkers.push(newMarker)
+      const newPosition = newMarker.getLatLng();
+      inputMarker.x = newPosition.lng;
+      inputMarker.y = newPosition.lat;
+      this.onMarkerDragEnd.emit(inputMarker);
+    });
+    newMarker.on('click', () => this.onMarkerClick.emit(inputMarker));
+    newMarker.addTo(this.lfMap);
+    this.leafletMarkers.push(newMarker);
   }
 
   async loadMap(mapUrl: string) {
-    const [width, height] = await this.imgWidthAndHeight(mapUrl)
-    const bounds = this.boundsFromImageSize(width, height)
-    imageOverlay(mapUrl, bounds).addTo(this.lfMap)
-    this.lfMap.fitBounds(bounds)
-    this.lfMap.setMaxBounds(bounds)
-    this.lfMap.on('click', this._onMapClick)
+    const [width, height] = await this.imgWidthAndHeight(mapUrl);
+    const bounds = this.boundsFromImageSize(width, height);
+    imageOverlay(mapUrl, bounds).addTo(this.lfMap);
+    this.lfMap.fitBounds(bounds);
+    this.lfMap.setMaxBounds(bounds);
+    this.lfMap.on('click', this._onMapClick);
   }
 
   imgWidthAndHeight(url: string): Promise<[number, number]> {
-    return new Promise<[number, number]>((resolve) => {
-      const img = new Image()
-      img.addEventListener('load', function(){
-        resolve([this.naturalWidth, this.naturalHeight])
+    return new Promise<[number, number]>(resolve => {
+      const img = new Image();
+      img.addEventListener('load', function () {
+        resolve([this.naturalWidth, this.naturalHeight]);
       });
-      img.src = url
+      img.src = url;
     })
   }
 
   boundsFromImageSize(imageWidth: number, imageHeight: number): [number, number][] {
-    const containerRatio = 673 / 337
-    const backgoundRatio = imageWidth / imageHeight
-
-    let width = 0
-    let height = 0
+    const containerRatio = 673 / 337;
+    const backgoundRatio = imageWidth / imageHeight;
+    let width = 0;
+    let height = 0;
     if (backgoundRatio > containerRatio) {
       // landscape
-      width = 1000
+      width = 1000;
       height = width / backgoundRatio;
     } else {
       // portrait
-      height = 1000
-      width = height * backgoundRatio
+      height = 1000;
+      width = height * backgoundRatio;
     }
-    return [[0, -width / 2], [height, width / 2]]
+    return [[0, -width / 2], [height, width / 2]];
   }
 }
 
-export interface Marker{
+export interface Marker {
   x: number
   y: number
   id: number
